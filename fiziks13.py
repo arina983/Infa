e884f93389c695772c65f7663950e970c4953954
Arina <arina.bubenina@mail.ru>
2025-10-31 16:05:09 +0700

    Update fiziks13.py

--- fiziks13.py
+++ fiziks13.py
@@ -2,19 +2,15 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-# Дано:
-A = 0.5  # амплитуда, мкКл
-beta = 0.1  # коэффициент затухания, 1/с
-omega = 100000 * math.pi  # частота, рад/с
-T = 2 * math.pi / omega  # период колебаний, с
+A = 0.5  
+beta = 0.1
+omega = 100000 * math.pi  
+T = 2 * math.pi / omega  
 
-# Время (от 0 до 2 периодов)
 t = np.linspace(0, 10 * T, 10000)
 
-# Уравнение затухающих колебаний
 q = A * np.exp(-beta * t) * np.cos(omega * t)
 
-# Построение графика
 plt.figure(figsize=(11, 6))
 ax = plt.gca()
 ax.spines['bottom'].set_position('zero')
@@ -23,12 +19,12 @@ ax.spines['top'].set_color('none')
 ax.grid(alpha=0.5)
 plt.plot(t, q, label='$q(t) = 0.5 e^{-0.1 t} \cos(10^4 \pi t)$', color='red')
 
-# Ограничение осей
-plt.xlim(0, 10 * T)  # График до двух периодов в секундах
+plt.xlim(0, 10 * T) 
 
 plt.title('График зависимости заряда от времени')
-plt.xlabel('t, секунд')  # Время в секундах
+plt.xlabel('t, секунд') 
 plt.ylabel('q, мкКл')
 plt.grid(True)
 plt.legend()
+
 plt.show()
