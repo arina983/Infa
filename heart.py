294508cc5057f4cf07c31c707e6c984eeeba2fa6
Arina <arina.bubenina@mail.ru>
2025-10-31 16:05:39 +0700

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,18 +1,16 @@
 import turtle
 import math
 
-# Настройка окна
 t = turtle.Turtle()
 t.speed(0)  
 t.color("red")  
 turtle.bgcolor("black")  
-# Функция для расчета координат сердца
+
 def corazon(n):
     x = 16 * math.sin(n) ** 3
     y = 13 * math.cos(n) - 5 * math.cos(2 * n) - 2 * math.cos(3 * n) - math.cos(4 * n)
     return x, y
 
-# Рисуем сердца
 t.penup()
 for i in range(1, 17): 
     t.goto(0, 0)  
@@ -25,3 +23,4 @@ for i in range(1, 17):
 t.hideturtle()  
 
 turtle.done() 
+
