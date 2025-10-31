9c1ba7cc0c2fc4a943f6a3e8e22b738bf082e98f
Arina <arina.bubenina@mail.ru>
2025-10-31 16:02:52 +0700

    Update fizik12.py

--- fizik12.py
+++ fizik12.py
@@ -2,23 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Дано:
-m = 0.1  # масса груза, кг
-k = 20  # жесткость пружины, Н/м
-A = 0.05  # амплитуда, м
-omega = np.sqrt(k / m)  # циклическая частота, рад/с
-phi_0 = 0.703  # начальная фаза, рад
-T = 2 * np.pi / omega  # период колебаний, с
+m = 0.1  
+k = 20 
+A = 0.05  
+omega = np.sqrt(k / m) 
+phi_0 = 0.703 
+T = 2 * np.pi / omega  
 
-# Время (от 0 до 2 периодов)
 t = np.linspace(0, 2 * T, 1000)
 
-# Потенциальная энергия
 E_pot = 0.5 * k * A**2 * np.cos(omega * t + phi_0)**2
 
-# Максимальное значение потенциальной энергии
 E_max = 0.5 * k * A**2
 
-# Построение графика
 plt.figure(figsize=(11, 6))
 ax = plt.gca()
 ax.spines['left'].set_position('zero')
@@ -27,11 +23,12 @@ ax.grid(alpha=0.5)
 plt.plot(t, E_pot, label=r'$E_{\text{пот}}(t) = \frac{1}{2} k A^2 \cos^2(\omega t + \varphi_0)$', color='orange')
 
 # Ограничение осей
-plt.xlim(0, 2 * T)  # График до двух периодов
+plt.xlim(0, 2 * T)  
 plt.ylim(0, E_max)
 plt.title('График зависимости потенциальной энергии от времени')
-plt.xlabel('t, секунд')  # Время в секундах
+plt.xlabel('t, секунд')
 plt.ylabel('E, Дж')
 plt.grid(True)
 plt.legend()
+
 plt.show()
